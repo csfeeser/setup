ff10c302cd622934ac787c0904835761447be441
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2023-04-13 15:56:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,70 +15,25 @@ kubectl get pods
 kubectl get pods -A
 kubectl apply -f https://raw.githubusercontent.com/ansible/awx-operator/0.10.0/deploy/awx-operator.yaml
 kubectl get pods
-vi awx-demo.yml
----
-apiVersion: awx.ansible.com/v1beta1
-kind: AWX
-metadata:
-  name: awx-demo
-spec:
-  service_type: nodeport
-  ingress_type: none
-  hostname: awx-demo.example.com
-```
-
-### Run the deployment:
-
-```
 kubectl apply -f awx-demo.yml
 kubectl get pods -l "app.kubernetes.io/managed-by=awx-operator"
 kubectl get svc -l "app.kubernetes.io/managed-by=awx-operator"
-```
-WAIT A FEW MINS...
+
+# WAIT A FEW MINS...
 
 ### Get the Admin user password:
-```
 kubectl get secrets
 kubectl get secret awx-demo-admin-password -o jsonpath="{.data.password}" | base64 --decode
-```
 
 ### Expose the deployment:
-```
+
 kubectl expose deployment awx-demo --type=LoadBalancer --port=8080
-```
 
 ### Minikube tunnel
-On a new session, start the minikube tunnel:
+# On a new session, start the minikube tunnel:
 
-```minikube tunnel```
+minikube tunnel
 
-### Enable AWX to be access via the Internet:
-```kubectl port-forward svc/awx-demo-service --address 0.0.0.0 30886:80```
+kubectl port-forward svc/awx-demo-service --address 0.0.0.0 30886:80
 
-Now visit https://your_ip:high_port
-
-**You may need to update your FW rules to be able to connect to the AWX login screen**
-
----
-**---** **ISSUES SECTION** **---**
-
-1) Starting minikube tunnel - Exiting due to GUEST_STATUS: state: unknown state "minikube": docker container inspect minikube --format=: exit status 1
-ANS: ```sudo chmod 666 /var/run/docker.sock ; sudo usermod -aG docker ${USER}```
-
-2) If you see the message: ImagePullBackOff or ErrImagePull when you run kubectl get pods, run the following command to see what the issue is:
-````
-kubectl describe pods <my-pod> # Output from "kubectl get pods" command for the pod with the issue.
-````
-I've seen a few issues where the server runs out of space so it should be easy to fix.
-
-3) If you have PENDING resources, try running a describe of the resource. If you see this at the bottom you are lacking in resources:
-( ```kubectl describe pods <my-pod>``` # Output from ```kubectl get pods``` )
-```
-Events:
-  Type     Reason            Age                 From               Message
-  ----     ------            ----                ----               -------
-  Warning  FailedScheduling  14s (x19 over 23m)  default-scheduler  0/1 nodes are available: 1 Insufficient cpu, 1 Insufficient memory.
-```
-
-This link has some more information:
-https://containersolutions.github.io/runbooks/posts/kubernetes/0-nodes-available-insufficient/
+# Now visit https://your_ip:high_port
